d34da6e58951a1f78ebc53bdc9c7703936212bcb
jayGattusoNLNZ <jay.gattuso@dia.govt.nz>
2020-07-08 10:21:45 +1200

    added holding note lookup

--- ui_test.py
+++ ui_test.py
@@ -2,7 +2,16 @@ from tkinter import *
 from openpyxl import Workbook, load_workbook
 import re
 import pyperclip
+from bs4 import BeautifulSoup as Soup
+import requests
+import configparser
+import textwrap
 
+
+config = configparser.ConfigParser()
+secrets = r"c:/source/secrets"
+config.read(secrets)
+apikey = config.get("configuration", "PRODUCTION")
 
 
 class AutocompleteEntry(Entry):
@@ -15,8 +24,8 @@ class AutocompleteEntry(Entry):
         
         self.mms_label  = StringVar()
 
-        self.title_body = Label(root, width=30, height=3, text="", textvariable=self.mms_label)
-        self.title_body.grid(row=1)
+        self.title_body = Label(root, width=width, height=6, text="", textvariable=self.mms_label)
+        self.title_body.grid(row=1, columnspan=3)
         self.var.trace('w', self.changed)
         self.bind("<Right>", self.selection)
         self.bind("<Up>", self.up)
@@ -57,7 +66,6 @@ class AutocompleteEntry(Entry):
             self.mms_label.set( f"{titles_lookup[self.var.get()]['mms']}") 
 
     def up(self, event):
-
         if self.lb_up:
             if self.lb.curselection() == ():
                 index = '0'
@@ -70,7 +78,6 @@ class AutocompleteEntry(Entry):
                 self.lb.activate(index) 
 
     def down(self, event):
-
         if self.lb_up:
             if self.lb.curselection() == ():
                 index = '0'
@@ -81,21 +88,16 @@ class AutocompleteEntry(Entry):
                 index = str(int(index)+1)        
                 self.lb.selection_set(first=index)
                 self.lb.activate(index) 
-
-
                 print ("1", self.var.get())
-
-                # self.title = self.var.get()
-                # self.mms_text.delete('1.0', '2.0')
-                # self.mms_text.insert('end',"some text" )
 
     def comparison(self):
         pattern = re.compile('.*' + self.var.get() + '.*', re.IGNORECASE)
         return [w for w in self.lista if re.match(pattern, w)]
 
 
-def get_titles_lookup():
+def get_title_and_mms_lookup():
 	titles_lookup = {}
+	mms_lookup = {}
 	len_data = 11
 	my_data = []
 	try:
@@ -122,7 +124,7 @@ def get_titles_lookup():
 
 	for row in my_data:
 		if row != [None, None, None, None, None, None, None]:
-			### these are all gubby excel fixes. :( 
+			### these are all grubby excel fixes. :( 
 			mms = row[1]
 			if str(mms).endswith("0"):
 				mms+=6
@@ -134,27 +136,63 @@ def get_titles_lookup():
 				wn += 36
 			
 			titles_lookup[row[0]] = {"mms":mms, "ATL":atl, "WN":wn, "pol":row[2], "title":row[0], 'signed_off':row[6]}
+			mms_lookup[mms] = {"mms":mms, "ATL":atl, "WN":wn, "pol":row[2], "title":row[0], 'signed_off':row[6]}
 
-	return titles_lookup
+	return titles_lookup, mms_lookup
+
+def get_holding_note(mms):
+	my_pol = mms_lookup[mms]['pol']
+	url = f"https://api-ap.hosted.exlibrisgroup.com/almaws/v1/acq/po-lines/{my_pol}?apikey={apikey}"
+	r = requests.get(url).text
+	soup = Soup(r, 'lxml')
+	my_note = soup.find("receiving_note").text
+	
+	if my_note != None: 
+		my_note = my_note.strip()
+
+	if my_note == "" or my_note == None:
+		my_note = "No Note"
+
+	my_note = my_note.replace(". ", ".\n")
+	my_note_lines = my_note.split("\n")
+	note_cleaned_lines = [] 
+	for line in my_note_lines:
+		my_line = textwrap.wrap(line, width, break_long_words=False)
+		note_cleaned_lines.append("\n".join(my_line))
+
+	my_note = "\n".join(note_cleaned_lines)
+	return (my_note)
+
+def get_and_update_with_rcv_note():
+	try:
+		mms = titles_lookup[entry.var.get()]['mms']
+		my_note = get_holding_note(mms)
+		entry.mms_label.set( f"{my_note}") 
+	except KeyError:
+		entry.mms_label.set( f"\n\tNo MMS selected")
+
 
 def clipboard():
-	pyperclip.copy(titles_lookup[entry.var.get()]['mms'])
-
-
+	try:
+		pyperclip.copy(titles_lookup[entry.var.get()]['mms'])
+	except KeyError:
+		entry.mms_label.set( f"\n\tNo MMS selected")
 
 def quit():
 	root.destroy()
 
+
+
 if __name__ == '__main__':
-    root = Tk()
-
-    titles_lookup = get_titles_lookup()
-    lista = list(titles_lookup.keys())
-    entry = AutocompleteEntry(lista, root)
-    entry.grid(row=0, column=0)
-    
-    Button(root, text='Copy MMS to clipboard', command=clipboard).grid(row=2, column=0)
-    Button(root, text='Quit', command=quit).grid(row=2, column=1)
-
-
-    root.mainloop()
+	width = 60
+	root = Tk()
+	titles_lookup, mms_lookup = get_title_and_mms_lookup()
+	lista = list(titles_lookup.keys())
+	entry = AutocompleteEntry(lista, root)
+	entry.grid(row=0, column=0, columnspan=3)
+	Button(root, text='Copy MMS to clipboard', command=clipboard).grid(row=2, column=0)
+	Button(root, text='Get Rcv Note', command=get_and_update_with_rcv_note).grid(row=2, column=1)
+	Button(root, text='Quit', command=quit).grid(row=2, column=2)
+	root.lift()
+	root.call('wm', 'attributes', '.', '-topmost', True)
+	root.mainloop()
